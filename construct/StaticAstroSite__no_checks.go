f91924671274465f9b086aa157451256deb2a1bc
helbing <helbingxxx@gmail.com>
2023-04-14 17:15:08 +0800

    chore(release): v0.0.5

---
+++ construct/StaticAstroSite__no_checks.go
@@ -0,0 +1,31 @@
+//go:build no_runtime_type_checking
+
+// The CDK Construct Library of Astro
+package construct
+
+// Building without runtime type checking enabled, so all the below just return nil
+
+func (s *jsiiProxy_StaticAstroSite) validateNewBucketParameters(scope constructs.Construct, staticDir *string, props *awss3.BucketProps) error {
+	return nil
+}
+
+func (s *jsiiProxy_StaticAstroSite) validateNewFunctionParameters(scope constructs.Construct, serverEntry *string, props *ServerOptions) error {
+	return nil
+}
+
+func (s *jsiiProxy_StaticAstroSite) validateNewS3OriginParameters(scope constructs.Construct, bucket awss3.Bucket) error {
+	return nil
+}
+
+func (s *jsiiProxy_StaticAstroSite) validateParseRoutesFromDirParameters(dir *string) error {
+	return nil
+}
+
+func validateStaticAstroSite_IsConstructParameters(x interface{}) error {
+	return nil
+}
+
+func validateNewStaticAstroSiteParameters(scope constructs.Construct, id *string, props *StaticAstroSiteProps) error {
+	return nil
+}
+
---
+++ construct/main.go
@@ -0,0 +1,117 @@
+package construct
+
+import (
+	"reflect"
+
+	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
+)
+
+func init() {
+	_jsii_.RegisterClass(
+		"@astrojs-aws/construct.AstroSiteConstruct",
+		reflect.TypeOf((*AstroSiteConstruct)(nil)).Elem(),
+		[]_jsii_.Member{
+			_jsii_.MemberMethod{JsiiMethod: "newBucket", GoMethod: "NewBucket"},
+			_jsii_.MemberMethod{JsiiMethod: "newFunction", GoMethod: "NewFunction"},
+			_jsii_.MemberMethod{JsiiMethod: "newS3Origin", GoMethod: "NewS3Origin"},
+			_jsii_.MemberProperty{JsiiProperty: "node", GoGetter: "Node"},
+			_jsii_.MemberMethod{JsiiMethod: "parseRoutesFromDir", GoMethod: "ParseRoutesFromDir"},
+			_jsii_.MemberMethod{JsiiMethod: "strToRuntime", GoMethod: "StrToRuntime"},
+			_jsii_.MemberMethod{JsiiMethod: "toString", GoMethod: "ToString"},
+		},
+		func() interface{} {
+			j := jsiiProxy_AstroSiteConstruct{}
+			_jsii_.InitJsiiProxy(&j.Type__constructsConstruct)
+			return &j
+		},
+	)
+	_jsii_.RegisterStruct(
+		"@astrojs-aws/construct.DistributionOptions",
+		reflect.TypeOf((*DistributionOptions)(nil)).Elem(),
+	)
+	_jsii_.RegisterClass(
+		"@astrojs-aws/construct.EdgeAstroSite",
+		reflect.TypeOf((*EdgeAstroSite)(nil)).Elem(),
+		[]_jsii_.Member{
+			_jsii_.MemberProperty{JsiiProperty: "bucketArn", GoGetter: "BucketArn"},
+			_jsii_.MemberProperty{JsiiProperty: "bucketName", GoGetter: "BucketName"},
+			_jsii_.MemberProperty{JsiiProperty: "distributionDomainName", GoGetter: "DistributionDomainName"},
+			_jsii_.MemberProperty{JsiiProperty: "domainName", GoGetter: "DomainName"},
+			_jsii_.MemberMethod{JsiiMethod: "edgeFunction", GoMethod: "EdgeFunction"},
+			_jsii_.MemberMethod{JsiiMethod: "newBucket", GoMethod: "NewBucket"},
+			_jsii_.MemberMethod{JsiiMethod: "newFunction", GoMethod: "NewFunction"},
+			_jsii_.MemberMethod{JsiiMethod: "newS3Origin", GoMethod: "NewS3Origin"},
+			_jsii_.MemberProperty{JsiiProperty: "node", GoGetter: "Node"},
+			_jsii_.MemberMethod{JsiiMethod: "parseRoutesFromDir", GoMethod: "ParseRoutesFromDir"},
+			_jsii_.MemberMethod{JsiiMethod: "strToRuntime", GoMethod: "StrToRuntime"},
+			_jsii_.MemberMethod{JsiiMethod: "toString", GoMethod: "ToString"},
+		},
+		func() interface{} {
+			j := jsiiProxy_EdgeAstroSite{}
+			_jsii_.InitJsiiProxy(&j.jsiiProxy_AstroSiteConstruct)
+			return &j
+		},
+	)
+	_jsii_.RegisterStruct(
+		"@astrojs-aws/construct.EdgeAstroSiteProps",
+		reflect.TypeOf((*EdgeAstroSiteProps)(nil)).Elem(),
+	)
+	_jsii_.RegisterClass(
+		"@astrojs-aws/construct.LambdaAstroSite",
+		reflect.TypeOf((*LambdaAstroSite)(nil)).Elem(),
+		[]_jsii_.Member{
+			_jsii_.MemberMethod{JsiiMethod: "api", GoMethod: "Api"},
+			_jsii_.MemberProperty{JsiiProperty: "bucketArn", GoGetter: "BucketArn"},
+			_jsii_.MemberProperty{JsiiProperty: "bucketName", GoGetter: "BucketName"},
+			_jsii_.MemberProperty{JsiiProperty: "distributionDomainName", GoGetter: "DistributionDomainName"},
+			_jsii_.MemberProperty{JsiiProperty: "domainName", GoGetter: "DomainName"},
+			_jsii_.MemberMethod{JsiiMethod: "lambdaFunction", GoMethod: "LambdaFunction"},
+			_jsii_.MemberMethod{JsiiMethod: "newBucket", GoMethod: "NewBucket"},
+			_jsii_.MemberMethod{JsiiMethod: "newFunction", GoMethod: "NewFunction"},
+			_jsii_.MemberMethod{JsiiMethod: "newS3Origin", GoMethod: "NewS3Origin"},
+			_jsii_.MemberProperty{JsiiProperty: "node", GoGetter: "Node"},
+			_jsii_.MemberMethod{JsiiMethod: "parseRoutesFromDir", GoMethod: "ParseRoutesFromDir"},
+			_jsii_.MemberMethod{JsiiMethod: "strToRuntime", GoMethod: "StrToRuntime"},
+			_jsii_.MemberMethod{JsiiMethod: "toString", GoMethod: "ToString"},
+		},
+		func() interface{} {
+			j := jsiiProxy_LambdaAstroSite{}
+			_jsii_.InitJsiiProxy(&j.jsiiProxy_AstroSiteConstruct)
+			return &j
+		},
+	)
+	_jsii_.RegisterStruct(
+		"@astrojs-aws/construct.LambdaAstroSiteProps",
+		reflect.TypeOf((*LambdaAstroSiteProps)(nil)).Elem(),
+	)
+	_jsii_.RegisterStruct(
+		"@astrojs-aws/construct.ServerOptions",
+		reflect.TypeOf((*ServerOptions)(nil)).Elem(),
+	)
+	_jsii_.RegisterClass(
+		"@astrojs-aws/construct.StaticAstroSite",
+		reflect.TypeOf((*StaticAstroSite)(nil)).Elem(),
+		[]_jsii_.Member{
+			_jsii_.MemberProperty{JsiiProperty: "bucketArn", GoGetter: "BucketArn"},
+			_jsii_.MemberProperty{JsiiProperty: "bucketName", GoGetter: "BucketName"},
+			_jsii_.MemberProperty{JsiiProperty: "distributionDomainName", GoGetter: "DistributionDomainName"},
+			_jsii_.MemberProperty{JsiiProperty: "domainName", GoGetter: "DomainName"},
+			_jsii_.MemberMethod{JsiiMethod: "newBucket", GoMethod: "NewBucket"},
+			_jsii_.MemberMethod{JsiiMethod: "newFunction", GoMethod: "NewFunction"},
+			_jsii_.MemberMethod{JsiiMethod: "newS3Origin", GoMethod: "NewS3Origin"},
+			_jsii_.MemberProperty{JsiiProperty: "node", GoGetter: "Node"},
+			_jsii_.MemberMethod{JsiiMethod: "parseRoutesFromDir", GoMethod: "ParseRoutesFromDir"},
+			_jsii_.MemberMethod{JsiiMethod: "strToRuntime", GoMethod: "StrToRuntime"},
+			_jsii_.MemberMethod{JsiiMethod: "toString", GoMethod: "ToString"},
+		},
+		func() interface{} {
+			j := jsiiProxy_StaticAstroSite{}
+			_jsii_.InitJsiiProxy(&j.jsiiProxy_AstroSiteConstruct)
+			return &j
+		},
+	)
+	_jsii_.RegisterStruct(
+		"@astrojs-aws/construct.StaticAstroSiteProps",
+		reflect.TypeOf((*StaticAstroSiteProps)(nil)).Elem(),
+	)
+}
